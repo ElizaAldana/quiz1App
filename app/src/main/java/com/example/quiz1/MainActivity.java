29ff9b89af119149b301e580b9be017bd2cb7405
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2021-03-12 11:53:09 -0500

    logré pasar el código que llevaba aquí

--- app/src/main/java/com/example/quiz1/MainActivity.java
+++ app/src/main/java/com/example/quiz1/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.quiz1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mainBtn;
+    private TextView strNombre, intId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainBtn = findViewById(R.id.mainBtn);
+        strNombre = findViewById(R.id.strNombre);
+        intId = findViewById(R.id.intId);
+
+        mainBtn.setOnClickListener(this);
+
+    }
+
+
+    public void onClick(View v) {
+        Intent a = new Intent(this, Registro.class);
+        startActivity(a);
     }
 }
--- app/src/main/java/com/example/quiz1/Registro.java
+++ app/src/main/java/com/example/quiz1/Registro.java
@@ -2,13 +2,40 @@ package com.example.quiz1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Registro extends AppCompatActivity {
+public class Registro extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText nombre, id;
+    private Button regBtn;
+    private String name, ident;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        regBtn = findViewById(R.id.regBtn);
+        nombre = findViewById(R.id.nombre);
+        id = findViewById(R.id.id);
+
+        regBtn.setOnClickListener(this);
+
+
+    }
+
+    public void onClick(View view) {
+
+        //los toma y los vuelve String
+        name = nombre.getText().toString();
+        ident = id.getText().toString();
+
+        Intent r = new Intent(this, EncuestaNE.class);
+        startActivity(r);
     }
 }
